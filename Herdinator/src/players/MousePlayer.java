8d3594bd2ac9ab1a71e7e9054e2791ea265275b8
Roland Meertens <rolandmeertens@hotmail.com>
2012-12-14 12:01:12 +0100

    Added a few sheep sounds

---
+++ Herdinator/src/players/MousePlayer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package players;
+
+import base.Player;
+
+/**
+ *
+ * @author roland
+ */
+public class MousePlayer extends Player{
+
+    
+    public MousePlayer(int id){
+        super(id);
+    }
+}
