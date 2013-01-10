0cf212bdb9f1360a490f84c58caecfbc057fe444
baskooiker <baskooiker90@gmail.com>
2013-01-10 16:48:00 +0100

    App is up and running. Communication to server works correctly.

--- Herdinator_mobile/src/com/example/herdinator_mobile/SettingsActivity.java
+++ Herdinator_mobile/src/com/example/herdinator_mobile/SettingsActivity.java
@@ -1,7 +1,6 @@
 package com.example.herdinator_mobile;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -15,15 +14,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -36,27 +34,42 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_layout);     
-        
-        String default_url = "http://wlan-133-119.wlan.ru.nl:8080";
-//        String default_url = "http://127.0.0.1:8080";
-//        String default_url = "http://10.0.2.2:8080";
-//        String default_url = "10.0.2.2:8080";
-        
-        EditText server_adress = (EditText) findViewById(R.id.server_adress);
-        server_adress.setText(default_url);
-        
-        Button connect_button = (Button) findViewById(R.id.connect_button);
-        connect_button.setOnClickListener(new ConnectClickListener(server_adress));
-        
-        
-    }
-    
-    private class ConnectClickListener implements OnClickListener{
-    	private EditText server_adress;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_layout);
+
+		// Network url
+		// String default_url = "http://wlan-133-119.wlan.ru.nl:8080";
+
+		// AVD url
+		String default_url = "http://10.0.2.2:8080";
+
+		EditText server_adress = (EditText) findViewById(R.id.server_adress);
+		server_adress.setText(default_url);
+
+		Button connect_button = (Button) findViewById(R.id.connect_button);
+		connect_button.setOnClickListener(new ConnectClickListener(
+				server_adress));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+		TextView error = (TextView) findViewById(R.id.error_field);
+		HerdinatorApplication app = (HerdinatorApplication) getApplication();
+
+		progress.setVisibility(View.INVISIBLE);
+
+		if (!app.isConnected() && app.getPhone_id() != null)
+			error.setText("Diconnected...");
+		else
+			error.setText("");
+	}
+
+	private class ConnectClickListener implements OnClickListener {
+		private EditText server_adress;
 
 		public ConnectClickListener(EditText server_adress) {
 			this.server_adress = server_adress;
@@ -65,88 +78,39 @@ public class SettingsActivity extends Activity {
 		@Override
 		public void onClick(View v) {
 			final ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
-			final TextView error = (TextView)findViewById(R.id.error_field);
-			
+			final TextView error = (TextView) findViewById(R.id.error_field);
+
 			EditText marker_id = (EditText) findViewById(R.id.marker_id);
 			String markString = marker_id.getText().toString();
 			final int markID;
-			if(markString.isEmpty()){
+			if (markString.isEmpty()) {
 				error.setText("Enter marker ID");
 				return;
 			}
-			else
-				markID = Integer.valueOf(markString);
-			
+
+			markID = Integer.valueOf(markString);
+			progress.setVisibility(View.VISIBLE);
 			String urlString = server_adress.getText().toString();
-			
-			error.setText("start!");
-			progress.setVisibility(View.VISIBLE);
 
-			error.setText("thread!");
-			Thread connectThread = new Thread(new RunConnect(markID, urlString){}); 
-			connectThread.start();
+			List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+			params.add(new BasicNameValuePair("action", "connect"));
+			params.add(new BasicNameValuePair("markId", String.valueOf(markID)));
 
-			error.setText("Connected!");
-			progress.setVisibility(View.INVISIBLE);
-		}
-    }
-    
-    private class RunConnect implements Runnable{
-    	private int markID;
-		private String urlString;
+			JSONObject map = Connection.getResponse(getApplicationContext(),
+					urlString, params);
 
-		public RunConnect(int markID, String urlString){
-    		this.markID = markID;
-    		this.urlString = urlString;
-    	}
-    	
-    	@Override
-		public void run() {
-			ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
-			TextView error = (TextView)findViewById(R.id.error_field);
-			HttpResponse response = null;
-			String s= "";
-			try {
+			if ((Boolean) map.get("success")) {
+				HerdinatorApplication app = (HerdinatorApplication) getApplication();
+				app.setPhone_id((String) map.get("phoneId"));
+				app.setConnected(true);
+				app.setUrl(urlString);
 
-				List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-				params.add(new BasicNameValuePair("action", "connect"));
-				params.add(new BasicNameValuePair("markId", String.valueOf(markID)));
-				String paramString = URLEncodedUtils.format(params, "utf-8");
-				
-				HttpGet httpGet = new HttpGet(urlString + "/?"+ paramString);
-				DefaultHttpClient httpClient = new DefaultHttpClient();
-
-				
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "errHttp String");
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err "+httpGet.getURI().toASCIIString() );
-
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err before execute");
-				response = httpClient.execute(httpGet);
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err after execute");
-				HttpEntity entity = response.getEntity();
-				s = EntityUtils.toString(entity);
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err "+s);
-				
-			} catch (ClientProtocolException e) {
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err ClientProtocolException");
-			} catch (IOException e) {
-				Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err IOException");
-			} 
-
-			Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err before json parse");
-			JSONObject map = (JSONObject) JSONValue.parse(s);
-			Logger.getLogger( SettingsActivity.class.getName() ).log( Level.WARNING, "err after parse");
-			try {
-				if(map.getBoolean("success")){
-					((HerdinatorApplication) getApplication()).setPhone_id(map.getInt("phoneId"));
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
+				Intent intent = new Intent(getBaseContext(), MainActivity.class);
+				startActivity(intent);
+			} else {
+				error.setText("Unable to connect to server...");
 			}
-			progress.setVisibility(View.VISIBLE);
-			
-			((HerdinatorApplication)getApplication()).setConnected(true);
-			
+
 		}
-    }
+	}
 }
