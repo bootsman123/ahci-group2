03d597d17aad95bca5c7fef36d6dd9f25f035293
Bas Bootsma <bootsman@gmail.com>
2013-01-17 00:30:02 +0100

    Fixed merge issues.

--- Herdinator/src/main/Main.java
+++ Herdinator/src/main/Main.java
@@ -18,7 +18,6 @@ public class Main
      */
     public static void main( String[] args )
     {
-      
         try
         {
             AppGameContainer app = new AppGameContainer( new Game() );
